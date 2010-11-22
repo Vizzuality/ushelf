5475ce0bca7206b745c252be018f8147c8584ea1
Ivica Cvetanovski <itchy@Ivica-Cvetanovskis-Unibody-MacBook-Pro-17-inch.local>
2010-11-22 17:05:56 +0100

    added feature attributes in seeds.rb, feature attributes in Refinery/Settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,36 @@ ref:string
 status:string
 submission_date:string
 area:string
+limit:string
+firstname:string
+lastname:string
+address:string
+zip:string
+city:string
+state:string
+ratification:string
+geology_of_the_area:string
+resources_of_the_area:string
+maritime_transport_through_the_area:string
+harbors_in_the_area:string
+climate:change:text
+eutrophication:text
+hazardous_substances:text
+radioactive_substances:text
+offshore_and_gas_industry:text
+fishing:text
+other_human_uses_and_impacts:text
+biodiversity_and_ecosystems:text
+order_of_submission:string
+date_of_submission:string
+comment:text
+communications_received_with_regard_to_the_submission:text
+provisions_of_article_76:text
+maximal_water_depth_in_metres_in_submission_area:string
+average_water_depth_in_metres_in_submission_area:string
+status_of_preparation:text
+intended_date_of_making_a_submission:string
+communication_from_other_states:string
 ATTRIBUTES
 
 RefinerySetting.set(:feature_attributes, attributes)
